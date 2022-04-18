82e67d58188704284e4e7e1ec2c6f5b67ecb5ae4
Camilo Higuita <chiguitar@unal.edu.co>
2022-04-18 17:21:13 -0500

    make cask app domain uppercase

--- src/main.cpp
+++ src/main.cpp
@@ -237,7 +237,7 @@ int main(int argc, char *argv[])
     //    qputenv("QT_AUTO_SCREEN_SCALE_FACTOR", "1");
 
     QGuiApplication app(argc, argv);
-    app.setOrganizationName("maui");
+    app.setOrganizationName(QStringLiteral("Maui"));
     app.setWindowIcon(QIcon("qrc:/mauikit-logo.png"));
 
     MauiApp::instance()->setIconName("qrc:/mauikit-logo.png");
