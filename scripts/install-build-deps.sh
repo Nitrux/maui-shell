d0fe2e2ef32736bcc5ea5baaff873f585d0260b0
Probal Basak <probal31@gmail.com>
2022-05-15 07:55:23 +0530

    Build maui shell appimage implementation using Github Actions (#31)
    * Recipe for building maui shell appimage
    
    * Scripts for installing dependencies
    
    * Updated gitignore for appimage
    
    * Configure github actions for building appimage
    
    * Configure github actions for building appimage
    
    * Build mauikit from master for appimage build with actions
    
    * Removing packages used for debugging
    
    * Enable workflow run with workflow_dispatch
    
    * Corrected typo in branch name
    
    * Removed workflow_dispatch event from actions
    
    * Updated packages
    
    * Added runtime dependency for sqlite
    
    * Trigger build on push to master

---
+++ scripts/install-build-deps.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]; then
+    APT_COMMAND="sudo apt-get"
+else
+    APT_COMMAND="apt-get"
+fi
+
+$APT_COMMAND update -q
+$APT_COMMAND install -qy --no-install-recommends \
+    cmake \
+    git \
+    build-essential \
+    extra-cmake-modules \
+    gtk-update-icon-cache \
+    qtbase5-dev \
+    qtdeclarative5-dev \
+    qtmultimedia5-dev \
+    qtquickcontrols2-5-dev \
+    libkf5coreaddons-dev \
+    libqt5svg5-dev \
+    libkf5config-dev \
+    libkf5i18n-dev \
+    libkf5bookmarks-dev \
+    libkf5widgetsaddons-dev \
+    libkf5completion-dev \
+    libkf5itemviews-dev \
+    libkf5jobwidgets-dev \
+    libkf5solid-dev \
+    libkf5xmlgui-dev \
+    libkf5configwidgets-dev \
+    libkf5auth-dev \
+    libkf5codecs-dev \
+    libkf5windowsystem-dev \
+    libkf5parts-dev \
+    libkf5pty-dev \
+    libkf5textwidgets-dev \
+    libkf5sonnet-dev \
+    libkf5service-dev \
+    gettext \
+    libkf5notifications-dev \
+    qtpositioning5-dev \
+    libqt5x11extras5-dev \
+    libxcb-shape0-dev \
+    libxcb-icccm4-dev \
+    libkf5doctools-dev \
+    libkf5runner-dev \
+    libkf5notifyconfig-dev \
+    libkf5su-dev \
+    libkf5wallet-dev \
+    libkf5idletime-dev \
+    libkf5declarative-dev \
+    libkf5crash-dev \
+    libkf5globalaccel-dev \
+    libkf5wayland-dev \
+    libkf5people-dev \
+    libkf5activitiesstats-dev \
+    libkf5kio-dev \
+    libkf5prison-dev \
+    libkf5package-dev \
+    libkf5archive-dev \
+    libkf5iconthemes-dev \
+    libkf5unitconversion-dev \
+    libkf5itemmodels-dev \
+    libkf5texteditor-dev \
+    libkf5syntaxhighlighting-dev \
+    libkf5activities-dev \
+    kinit-dev \
+    libqt5waylandcompositor5-dev \
+    kded5-dev \
+    libpolkit-agent-1-dev \
+    libpolkit-qt5-1-dev \
+    libfontconfig1-dev \
+    libphonon4qt5-dev
---
+++ scripts/install-icon-theme.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git clone https://github.com/Nitrux/luv-icon-theme.git --depth=1
+
+cd luv-icon-theme
+mkdir -p "$APPDIR"/usr/share/icons/hicolor/ && cp -vrf Luv/* "$APPDIR"/usr/share/icons/hicolor/
+rm "$APPDIR"/usr/share/icons/hicolor/icon-theme.cache
