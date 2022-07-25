a9048641aca3571868dc529a06bb0e41c4256597
Probal Basak <probal31@gmail.com>
2022-07-25 21:44:30 +0530

    Added script to install cask server for appimage build

---
+++ scripts/deploy-cask-server.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+CASK_SERVER_VERSION=$([ -z $BRANCH_CASK_SERVER ] && echo "master" || echo $BRANCH_CASK_SERVER)
+
+echo "Cloning Cask Server from branch $CASK_SERVER_VERSION"
+
+git clone https://github.com/Nitrux/cask-server.git --depth=1 -b $CASK_SERVER_VERSION
+
+cd cask-server
+cmake -DCMAKE_BUILD_TYPE="Release" \
+    -DCMAKE_INSTALL_PREFIX="/usr" \
+    -DBUNDLE_MAUI_STYLE=ON \
+    .
+
+make -j$(nproc)
+DESTDIR=/ make install
+make install
