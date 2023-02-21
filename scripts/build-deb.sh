d05652cf74650eed8659aeb9b0eedd820c7d1956
UriHerrera <hello@uriherrera.com>
2023-02-21 17:50:21 -0600

    update

--- scripts/build-deb.sh
+++ scripts/build-deb.sh
@@ -4,18 +4,15 @@ set -x
 
 ### Update sources
 
-wget -qO /etc/apt/sources.list.d/nitrux-main-compat-repo.list https://raw.githubusercontent.com/Nitrux/iso-tool/development/configs/files/sources.list.nitrux
+wget -qO /etc/apt/sources.list.d/nitrux-depot.list https://raw.githubusercontent.com/Nitrux/iso-tool/legacy/configs/files/sources/sources.list.nitrux
 
-wget -qO /etc/apt/sources.list.d/nitrux-testing-repo.list https://raw.githubusercontent.com/Nitrux/iso-tool/development/configs/files/sources.list.nitrux.testing
+wget -qO /etc/apt/sources.list.d/nitrux-testing.list https://raw.githubusercontent.com/Nitrux/iso-tool/legacy/configs/files/sources/sources.list.nitrux.testing
 
-DEBIAN_FRONTEND=noninteractive apt-key adv --keyserver keyserver.ubuntu.com --recv-keys \
-	55751E5D > /dev/null
-
-curl -L https://packagecloud.io/nitrux/repo/gpgkey | apt-key add -;
-curl -L https://packagecloud.io/nitrux/compat/gpgkey | apt-key add -;
+curl -L https://packagecloud.io/nitrux/depot/gpgkey | apt-key add -;
+curl -L https://packagecloud.io/nitrux/unison/gpgkey | apt-key add -;
 curl -L https://packagecloud.io/nitrux/testing/gpgkey | apt-key add -;
 
-DEBIAN_FRONTEND=noninteractive apt -qq update
+apt -qq update
 
 ### Install Package Build Dependencies #2
 
@@ -50,6 +47,8 @@ cmake \
 
 make -j$(nproc)
 
+make install
+
 ### Run checkinstall and Build Debian Package
 
 >> description-pak printf "%s\n" \
@@ -75,7 +74,7 @@ checkinstall -D -y \
 	--pakdir=. \
 	--maintainer=uri_herrera@nxos.org \
 	--provides=maui-shell \
-	--requires="agenda-git \(\>= 2.2.0+git\),bluedevil,cask-server-git,kded5,kinit,libc6,libfontconfig1,libkf5activities5,libkf5activitiesstats1,libkf5archive5,libkf5configcore5,libkf5coreaddons5,libkf5crash5,libkf5declarative5,libkf5doctools5,libkf5i18n5,libkf5idletime5,libkf5itemmodels5,libkf5kiocore5,libkf5notifications5,libkf5notifyconfig5,libkf5package5,libkf5people5,libkf5prison5,libkf5runner5,libkf5runner5,libkf5service5,libkf5su5,libkf5syntaxhighlighting5,libkf5texteditor5,libkf5unitconversion5,libkf5wallet5,libkf5waylandclient5,libkf5waylandserver5,libphonon4qt5-4,libpolkit-agent-1-0,libpolkit-agent-1-0,libpolkit-qt5-1-1,libqt5core5a,libqt5gui5,libqt5qml5,libqt5svg5,libqt5waylandcompositor5,libqt5widgets5,libstdc++6,libwayland-bin,libwayland-client0,libwayland-cursor0,libwayland-egl1,libwayland-server0,maui-core-git \(\>= 2.2.0+git\),mauikit-filebrowsing-git \(\>= 2.2.0+git\),mauikit-git \(\>= 2.2.0+git\),plasma-nm,plasma-pa,qml-module-qt-labs-calendar,qml-module-qt-labs-platform,qml-module-qtwayland-compositor,qtwayland5" \
+	--requires="agenda-git \(\>= 2.2.2+git\),bluedevil,cask-server-git,kded5,kinit,libc6,libfontconfig1,libkf5activities5,libkf5activitiesstats1,libkf5archive5,libkf5configcore5,libkf5coreaddons5,libkf5crash5,libkf5declarative5,libkf5doctools5,libkf5i18n5,libkf5idletime5,libkf5itemmodels5,libkf5kiocore5,libkf5notifications5,libkf5notifyconfig5,libkf5package5,libkf5people5,libkf5prison5,libkf5runner5,libkf5runner5,libkf5service5,libkf5su5,libkf5syntaxhighlighting5,libkf5texteditor5,libkf5unitconversion5,libkf5wallet5,libkf5waylandclient5,libkf5waylandserver5,libphonon4qt5-4,libpolkit-agent-1-0,libpolkit-agent-1-0,libpolkit-qt5-1-1,libqt5core5a,libqt5gui5,libqt5qml5,libqt5svg5,libqt5waylandcompositor5,libqt5widgets5,libstdc++6,libwayland-bin,libwayland-client0,libwayland-cursor0,libwayland-egl1,libwayland-server0,maui-core-git \(\>= 2.2.2+git\),mauikit-filebrowsing-git \(\>= 2.2.2+git\),mauikit-git \(\>= 2.2.2+git\),plasma-nm,plasma-pa,qml-module-qt-labs-calendar,qml-module-qt-labs-platform,qml-module-qtwayland-compositor,qtwayland5" \
 	--nodoc \
 	--strip=no \
 	--stripso=yes \
