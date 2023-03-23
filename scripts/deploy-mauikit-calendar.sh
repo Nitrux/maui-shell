11887ef9ead13415f5f91255ececcc8780d0da49
UriHerrera <hello@uriherrera.com>
2023-03-23 16:20:57 -0600

    fix  var name

---
+++ scripts/deploy-mauikit-calendar.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+MAUIKIT_CALENDAR_VERSION=$([ -z $BRANCH_MAUIKIT_CALENDAR ] && echo "main" || echo $BRANCH_MAUIKIT_CALENDAR)
+
+echo "Cloning Maui Calendar from branch $MAUIKIT_CALENDAR_VERSION"
+
+git clone https://invent.kde.org/maui/mauikit-calendar.git --depth=1 -b $MAUIKIT_CALENDAR_VERSION
+
+cd mauikit-calendar
+cmake -DCMAKE_BUILD_TYPE="Release" \
+    -DCMAKE_INSTALL_PREFIX="/usr" \
+    -DBUNDLE_MAUI_STYLE=ON \
+    .
+
+make -j$(nproc)
+DESTDIR=/ make install
+make install
