17d440f65b9ee324235455813a13e450a1639d56
UriHerrera <hello@uriherrera.com>
2023-03-23 20:38:08 -0600

    link sh

--- scripts/install-icon-theme.sh
+++ scripts/install-icon-theme.sh
@@ -5,3 +5,4 @@ git clone https://github.com/Nitrux/luv-icon-theme.git --depth=1
 cd luv-icon-theme
 mkdir -p "$APPDIR"/usr/share/icons/hicolor/ && cp -vrf Luv/* "$APPDIR"/usr/share/icons/hicolor/
 rm "$APPDIR"/usr/share/icons/hicolor/icon-theme.cache
+update-icon-caches /usr/share/icons/*
